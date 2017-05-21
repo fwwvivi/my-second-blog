8495dbc5f85756715a146211433fb31882afcaf2
fwwvivi <fwwvivi@gmail.com>
2017-05-21 10:32:21 +0800

    Modified templates to display posts from database.

---
+++ blog/migrations/0003_auto_20170521_1020.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_auto_20170517_1346'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='post',
+            old_name='create_date',
+            new_name='created_date',
+        ),
+    ]
